e94d7dc719de9d3de6945af57a9bed050eb07e07
jessicaly666 <yan.li@vcg.com>
2022-01-10 10:41:17 +0800

    init

---
+++ dashboard/src/App.tsx
@@ -0,0 +1,7 @@
+import { Switch, Route } from '@modern-js/runtime/router';
+
+import './App.css';
+
+const App = props => <div>dashboard {props.username}</div>;
+
+export default App;
---
+++ tablelist/src/App.tsx
@@ -0,0 +1,7 @@
+import { Switch, Route } from '@modern-js/runtime/router';
+
+import './App.css';
+
+const App = () => <div>table app</div>;
+
+export default App;
